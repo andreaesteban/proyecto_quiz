d548a1a1687b04838f23b231a89a5f965fde19dc
andreaesteban <andreaestean19@hotmail.com>
2015-05-17 23:13:50 +0200

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,28 +1,20 @@
 var models = require('../models/models.js');
 
-// GET /quizes/:id
-exports.show = function(req,res) { //para la lista de preguntas
-	models.Quiz.find(req.params.quizId).then(function (quiz) {
- 		res.render('quizes/show', {quiz: quiz});
- 	})
-};
-
-// GET /quizes/answer
-exports.answer = function(req, res) {
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-		if (req.query.respuesta === quiz.respuesta){
-			res.render('quizes/answer', {respuesta: '¡Que listo eres!', 
-				quizId: req.params.quizId});
-		}else {
-			res.render('quizes/answer', {respuesta: 'Maaaal', 
-				quizId: req.params.quizId});
+//Autoload -factoriza el codigo sin ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(
+		function(quiz){
+			if(quiz){
+				req.quiz = quiz;
+				next();
+			}else{next(new Error('No existe quizId=' + quizId));}
 		}
-	})
+	).catch(function(error){next(error);});
 };
 
 //GET /quizes
 //GET /quizes?search=texto_a_buscar
-exports.index = function(req,res) {
+/*exports.index = function(req,res) {
 	if (req.query.search != null) {
 		models.Quiz.findAll({where: ["pregunta like ?", '%'+req.query.search+'%'], order:'pregunta ASC'})
 		.then(function (quizes) {
@@ -33,7 +25,29 @@ exports.index = function(req,res) {
 			res.render('quizes/index', {quizes: quizes});
 		});
 	}
+};*/
+exports.index=function(req,res){
+		models.Quiz.findAll().then(
+			function(quizes){
+				res.render('quizes/index', {quizes: quizes});
+			}
+		).catch(function(error){next(error);})
 };
+
+// GET /quizes/:id
+exports.show = function(req,res) { //para la lista de preguntas
+ 	res.render('quizes/show', {quiz: req.quiz});
+};
+
+// GET /quizes/:id/answer
+exports.answer = function(req, res) {
+	var resultado = 'Maaaal';
+	if (req.query.respuesta === req.quiz.respuesta){
+		resultado='¡Que listo eres!';		
+	}
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+};
+
 //GET /author
 exports.author = function(req, res){
 	res.render('author', {autor: 'Andrea Esteban'})
