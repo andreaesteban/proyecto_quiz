729a2916fe00d241a5f254d8a9f1339c96381944
andreaesteban <andreaestean19@hotmail.com>
2015-05-18 00:31:18 +0200

    Crear pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,7 +14,7 @@ exports.load = function(req, res, next, quizId) {
 
 //GET /quizes
 //GET /quizes?search=texto_a_buscar
-/*exports.index = function(req,res) {
+exports.index = function(req,res) {
 	if (req.query.search != null) {
 		models.Quiz.findAll({where: ["pregunta like ?", '%'+req.query.search+'%'], order:'pregunta ASC'})
 		.then(function (quizes) {
@@ -25,14 +25,14 @@ exports.load = function(req, res, next, quizId) {
 			res.render('quizes/index', {quizes: quizes});
 		});
 	}
-};*/
-exports.index=function(req,res){
+};
+/*exports.index=function(req,res){
 		models.Quiz.findAll().then(
 			function(quizes){
 				res.render('quizes/index', {quizes: quizes});
 			}
 		).catch(function(error){next(error);})
-};
+};*/
 
 // GET /quizes/:id
 exports.show = function(req,res) { //para la lista de preguntas
@@ -46,6 +46,24 @@ exports.answer = function(req, res) {
 		resultado='¡Que listo eres!';		
 	}
 	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+};
+
+//GET /quizes/new
+exports.new = function(req,res){
+	var quiz= models.Quiz.build( //crea objeto quiz
+		{pregunta:"Pregunta", respuesta: "Respuesta"}
+	);
+	res.render('quizes/new', {quiz: quiz});
+};
+
+//POST /quizes/create
+exports.create = function(req,res){
+	var quiz = models.Quiz.build(req.body.quiz);
+
+	//guardar en DB los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	}) //Redireccion HTTP (URL relativo) lista de preguntas
 };
 
 //GET /author
--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,8 @@ router.param('quizId', quizController.load); //autoload :quizId
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
 
 //ruta para la pagina para el autor
 router.get('/author',                      quizController.author);
