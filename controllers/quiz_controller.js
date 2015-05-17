c7251e3c958b2ec55515b4fdc2c9496477a1dcde
andreaesteban <andreaestean19@hotmail.com>
2015-05-18 01:16:04 +0200

    Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -18,11 +18,11 @@ exports.index = function(req,res) {
 	if (req.query.search != null) {
 		models.Quiz.findAll({where: ["pregunta like ?", '%'+req.query.search+'%'], order:'pregunta ASC'})
 		.then(function (quizes) {
-			res.render('quizes/index', {quizes: quizes});
+			res.render('quizes/index', {quizes: quizes, errors: []});
 		});
 	}else {
 		models.Quiz.findAll().then(function (quizes) {
-			res.render('quizes/index', {quizes: quizes});
+			res.render('quizes/index', {quizes: quizes, errors: []});
 		});
 	}
 };
@@ -36,7 +36,7 @@ exports.index = function(req,res) {
 
 // GET /quizes/:id
 exports.show = function(req,res) { //para la lista de preguntas
- 	res.render('quizes/show', {quiz: req.quiz});
+ 	res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -45,7 +45,7 @@ exports.answer = function(req, res) {
 	if (req.query.respuesta === req.quiz.respuesta){
 		resultado='¡Que listo eres!';		
 	}
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors:[]});
 };
 
 //GET /quizes/new
@@ -53,21 +53,29 @@ exports.new = function(req,res){
 	var quiz= models.Quiz.build( //crea objeto quiz
 		{pregunta:"Pregunta", respuesta: "Respuesta"}
 	);
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
-//POST /quizes/create
-exports.create = function(req,res){
-	var quiz = models.Quiz.build(req.body.quiz);
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
 
-	//guardar en DB los campos pregunta y respuesta de quiz
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	}) //Redireccion HTTP (URL relativo) lista de preguntas
+	quiz
+	.validate()
+	.then(
+		function(err){
+			if (err) {
+				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+			} else {
+			quiz // save: guarda en DB campos pregunta y respuesta de quiz
+			.save({fields: ["pregunta", "respuesta"]})
+			.then( function(){ res.redirect('/quizes')})
+			} // res.redirect: Redirección HTTP a lista de preguntas
+		}
+	);
 };
-
 //GET /author
 exports.author = function(req, res){
-	res.render('author', {autor: 'Andrea Esteban'})
+	res.render('author', {autor: 'Andrea Esteban', errors: []})
 }
 
